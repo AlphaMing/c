e5162730c14a18f1f2fdaa5daf1eca11dae3a0c2
GM1 <32616789+AlphaMing@users.noreply.github.com>
2017-12-29 14:22:57 +0800

    Update 万年历.c

--- 万年历.c
+++ 万年历.c
@@ -1,24 +1,31 @@
-// unfinished
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<stdbool.h>
 int monthNum[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-int Firstday(int *, int *, int*);
+typedef struct _date {
+	int year;
+	int month;
+} Date;
+//algorithm
+int Algorithm(int *, int *, int*);
+//This function checks input
 _Bool error(char [50], char [50]);
 int main()
 {
+	/*
+	  flag records the first day of the month
+	  _flag records whether the year is a leap year
+	  _Flag records how many days the month has
+	  Year records the year
+	*/
 	int i, j, n, flag, _flag = 0, _Flag, Year;
-	flag = n = Firstday(&_Flag, &_flag, &Year);
+	flag = n = Algorithm(&_Flag, &_flag, &Year);
 	system("cls");
 	if (flag == 0)
 		flag = 7;
-	for (i = 0; i < 3; i++)
-		putchar('\t');
-	printf("万年历\n");
-	for (i = 0; i < 3; i++)
-		putchar('\t');
-	printf("%d年%d月", Year, _Flag);
+	printf("\t\t\t万年历\n");
+	printf("\t\t\t%d年%d月", Year, _Flag);
 	putchar('\n');
 	printf("星期一\t星期二\t星期三\t星期四\t星期五\t星期六\t星期天\n");
 	if (_flag && _Flag == 2)
@@ -57,17 +64,14 @@ _Bool error(char year[50], char month[50])
 	if (!(atoi(month) >= 0 && atoi(month) <= 12))
 		flag = false;
 	if(!flag)
-		printf("error!请重新输入！");
+		printf("error!请重新输入日期！");
 	return flag;
 }
-int Firstday(int *Month, int *_flag, int *_year)
+int Algorithm(int *Month, int *_flag, int *_year)
 {
-	struct _date {
-		int year;
-		int month;
-	} date;
+	Date date;
 	char input[50], year[50], month[50];
-	int i, sum = 0, flag = 0, temp = 1900;
+	int i, sum = 0, temp = 1900;
 	printf("Enter the date(e.g 2008 8):");
 	gets(input);
 	sscanf(input, "%s%s", year, month);
@@ -88,7 +92,6 @@ int Firstday(int *Month, int *_flag, int *_year)
 	for (i = temp; i < date.year; i++) {
 		if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0) {
 			sum += 366 % 7;
-			flag = 1;
 		}
 		else
 			sum += 365 % 7;
@@ -96,7 +99,7 @@ int Firstday(int *Month, int *_flag, int *_year)
 	for (i = 0; i < date.month - 1; i++)
 		sum += monthNum[i] % 7;
 	if (date.month > 2)
-		sum += flag;
+		sum += *_flag;
 	sum = (sum + 1) % 7;
 	return sum;
 }
